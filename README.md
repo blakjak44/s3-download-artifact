5a8c89a5cd171ce1f299e0fb0147a6c498e0e41f
Jack Zeitoun <blakjak44@gmail.com>
2022-11-22 13:48:03 -0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+## s3-download-artifact
+A Github Action for downloading artifacts from Amazon S3 buckets.
---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+import { S3ArtifactClient } from 's3-artifact'
+
+
+async function run(): Promise<void> {
+  const region = core.getInput('aws_region')
+  const accessKeyId = core.getInput('aws_access_key_id')
+  const secretAccessKey = core.getInput('aws_secret_access_key')
+  const bucket = core.getInput('bucket')
+  const scope = core.getInput('scope')
+  const name = core.getInput('name')
+  const path = core.getInput('path')
+
+  const client = new S3ArtifactClient(region, accessKeyId, secretAccessKey)
+
+  const result = await client.download(
+    bucket,
+    scope,
+    name,
+    path,
+    github.context
+  )
+
+  if (!result) {
+    core.setFailed('Failed to download artifact.')
+  } else {
+    core.info(`Artifact "${name}" with ${result.count} files was successfully downloaded to ${path}.`)
+  }
+}
+
+run()
